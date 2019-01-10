16e90a580a227ffa183e7c4f204103e385c07e9d
Salvatore Palazzo <salvatore.palazzo@ipsoft.com>
2019-01-10 18:51:44 +0100

    initial commit

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import random
 app = Flask(__name__, template_folder='templates')
 
 SCORE = {"player": 0, "IA": 0}
+WINNER = ""
 
 
 def name_to_number(name):
     if name == "rock":
         number = 0
-    elif name == "Spock":
+    elif name == "spock":
         number = 1
     elif name == "paper":
         number = 2
@@ -24,7 +25,7 @@ def number_to_name(number):
     if number == 0:
         name = "rock"
     elif number == 1:
-        name = "Spock"
+        name = "spock"
     elif number == 2:
         name = "paper"
     elif number == 3:
@@ -49,16 +50,14 @@ def play(player_choice):
 
     if result >= 3 and result <= 4:
         SCORE["IA"] += 1
-        print("IA wins!")
-        return "IA wins!"
+        WINNER = "IA wins!"
+        return WINNER
 
     elif result >= 1 and result <= 2:
         SCORE["player"] += 1
-        print("Player wins!")
         return "Player wins!"
 
     else:
-        print("It's a Tie!")
         return "It's a Tie!"
 
 
@@ -70,13 +69,13 @@ def player_input():
     if request.method == 'POST' and 'name' in request.form:
         name = request.form['name']
 
-    score = None
+    winner = None
 
     choice = request.args.get('choice')
     if choice:
-       score = play(choice)
+       winner = play(choice)
 
-    return render_template('index.html', name=name, choice=choice, score=score)
+    return render_template('index.html', name=name, choice=choice, winner=winner)
 
 
 if __name__ == '__main__':
