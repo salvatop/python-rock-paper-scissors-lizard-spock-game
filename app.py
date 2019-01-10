74c19e6b10d34533a9dff4ab4c7e2298a56dc2e7
Salvatore Palazzo <salvatore.palazzo@ipsoft.com>
2019-01-10 19:31:36 +0100

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 app = Flask(__name__, template_folder='templates')
 
 SCORE = {"player": 0, "IA": 0}
-WINNER = ""
+IA = {"ia" : None}
 
 
 def name_to_number(name):
@@ -37,21 +37,17 @@ def number_to_name(number):
 
 def play(player_choice):
     player_number = name_to_number(player_choice)
-    player_choice = number_to_name(player_number)
-    print("Player chooses %s" % (player_choice))
 
-    # generate computer choice
-    computer_choice = random.randrange(4)
-    comp_number = number_to_name(computer_choice)
-    print("IA chooses %s" % (comp_number))
+    comp_number = random.randrange(4)
+    computer_choice = number_to_name(comp_number)
+    IA["ia"] = computer_choice
 
     # using modulo to determine the winner
-    result = (player_number - computer_choice) % 5
+    result = (player_number - comp_number) % 5
 
     if result >= 3 and result <= 4:
         SCORE["IA"] += 1
-        WINNER = "IA wins!"
-        return WINNER
+        return "IA wins!"
 
     elif result >= 1 and result <= 2:
         SCORE["player"] += 1
@@ -73,9 +69,9 @@ def player_input():
 
     choice = request.args.get('choice')
     if choice:
-       winner = play(choice)
+        winner = play(choice)
 
-    return render_template('index.html', name=name, choice=choice, winner=winner)
+    return render_template('index.html', name=name, choice=choice, winner=winner, score=SCORE, ia=IA)
 
 
 if __name__ == '__main__':
